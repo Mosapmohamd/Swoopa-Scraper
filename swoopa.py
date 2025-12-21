76985de84d8403c7b50e6fc5a274c089e77f1dea
Mosap Abdel-Ghany <abdelghanymosap@gmail.com>
2025-12-21 18:48:37 +0200

    Update swoopa.py

--- swoopa.py
+++ swoopa.py
@@ -3,9 +3,9 @@ import requests
 
 app = FastAPI(title="ScrapingBee Proxy API")
 
-SCRAPINGBEE_API_KEY = "YOUR_API_KEY"
+SCRAPINGBEE_API_KEY = "YN1VF40U7FTTOS9C99DP667VGZ7I0NHAL29ON5TT4QLWAM2C8L9VEI6U0O3GN93CGF74X8KU1W558DQN"
 TARGET_URL = "https://backend.getswoopa.com/api/marketplace/"
-AUTH_TOKEN = "YOUR_BEARER_TOKEN"
+AUTH_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzY2Mzk3OTU5LCJpYXQiOjE3NjYzMTE1NTksImp0aSI6IjkzYjJmZDE1Y2NkMzRjNTJiZDg4MTdmYzMzMGNhZWU1IiwidXNlcl9pZCI6Ijk1MjE2In0.Mfoa3hcvQGBhjcmCcoaztbeCnKm4a7kpA8IIzIN7lAc"
 
 @app.get("/fetch-marketplace")
 def fetch_marketplace():
@@ -21,7 +21,7 @@ def fetch_marketplace():
             },
             headers={
                 "spb-accept": "application/json",
-                "spb-authorization": f"Bearer {AUTH_TOKEN}"
+                "spb-authorization": f"{AUTH_TOKEN}"
             },
             timeout=30
         )
@@ -33,3 +33,4 @@ def fetch_marketplace():
 
     except requests.exceptions.RequestException as e:
         raise HTTPException(status_code=500, detail=str(e))
+
