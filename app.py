71fcd82593bd3b1450c37ef0252e84f7d78f84da
Mosap Abdel-Ghany <abdelghanymosap@gmail.com>
2025-12-22 17:23:22 +0200

    Refactor app.py to improve code structure

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = FastAPI(title="ScrapingBee Proxy API")
 
 SCRAPINGBEE_API_KEY = "YN1VF40U7FTTOS9C99DP667VGZ7I0NHAL29ON5TT4QLWAM2C8L9VEI6U0O3GN93CGF74X8KU1W558DQN"
 TARGET_URL = "https://backend.getswoopa.com/api/marketplace/"
-AUTH_TOKEN = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzY2NDg5MDI4LCJpYXQiOjE3NjY0MDI2MjgsImp0aSI6ImNjY2U3N2JmNzM3NDQyNDdhMDVlYTY3OWUxYmZkYTA5IiwidXNlcl9pZCI6Ijk1MjE2In0.CYcd9nYbMpyuiMb4g3fTjokku59u4tzMRA9oK86eUSw"
+AUTH_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzY2NDg5MDI4LCJpYXQiOjE3NjY0MDI2MjgsImp0aSI6ImNjY2U3N2JmNzM3NDQyNDdhMDVlYTY3OWUxYmZkYTA5IiwidXNlcl9pZCI6Ijk1MjE2In0.CYcd9nYbMpyuiMb4g3fTjokku59u4tzMRA9oK86eUSw"
 
 SCRAPINGBEE_ENDPOINT = "https://app.scrapingbee.com/api/v1"
 
@@ -44,4 +44,5 @@ def fetch_marketplace():
 
     except requests.exceptions.RequestException as e:
         raise HTTPException(status_code=500, detail=str(e))
+
 
