adda44b0581a6c85ed6d4c76c1a959793ba0cacc
Mosap Abdel-Ghany <abdelghanymosap@gmail.com>
2025-12-23 21:41:01 +0200

    Update ScrapingBee API key and clean up code

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import requests
 
 app = FastAPI(title="ScrapingBee Proxy API")
 
-SCRAPINGBEE_API_KEY = "YN1VF40U7FTTOS9C99DP667VGZ7I0NHAL29ON5TT4QLWAM2C8L9VEI6U0O3GN93CGF74X8KU1W558DQN"
+SCRAPINGBEE_API_KEY = "4Z7NQS4HD6KZ37X8R46ACMJLZ0JG2INPMVNVSTRZJ9RUK5EE305BFU9XX1FFUGIEBY1UG7PIIQE3GA54"
 TARGET_URL = "https://backend.getswoopa.com/api/marketplace/"
 AUTH_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzY2NjAzNzg3LCJpYXQiOjE3NjY1MTczODcsImp0aSI6IjA3MzllZjhiNmZiNDQ3Y2FiMzllNDhiOWFiMWYwNzRkIiwidXNlcl9pZCI6Ijk1MjE2In0.EWRm0_Yn3Gv3px5TcWwh0klpMIV9S5jgKd9j2mFtC90"
 
@@ -44,6 +44,7 @@ def fetch_marketplace():
 
     except requests.exceptions.RequestException as e:
         raise HTTPException(status_code=500, detail=str(e))
+
 
 
 
